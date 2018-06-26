2cf104ac5b4f8564d8e01ab92ba53b4ad1d43b91
root <root@smtp.gmail.com>
2018-06-26 19:50:28 +0530

    initail commit

---
+++ venv1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+date
+#mkdir /home/sourav/python1
+cd /home/sourav/python1
+sudo virtualenv venv
+#usr/bin/source vev/bin/activate
+sudo pip install -r /home/sourav/requirements.txt
+python hello.py
+
